84fba1df33536a29d006fb7e9bb979c7609f8c76
Abdelhamid Bouazi <abouazi@e3r8p10.1337.ma>
2025-02-16 20:23:38 +0100

    errors in wordpress

--- src/services/wordpress/setup.sh
+++ src/services/wordpress/setup.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
+
 
 mkdir -p /run/php
 
@@ -9,19 +10,22 @@ mv wp-cli.phar /usr/local/bin/wp
 
 
 cd /var/www/html
-chmod -R 777 /var/www/html
-
+chmod 777 /var/www/html
 wp core download --allow-root
-
 cp wp-config-sample.php wp-config.php
-wp config set DB_NAME "$MYSQL_DATABASE" --allow-root
-wp config set DB_USER "$MYSQL_USER" --allow-root
-wp config set DB_PASSWORD "$MYSQL_PASSWORD" --allow-root
-wp config set DB_HOST "mysql" --allow-root
+wp config set DB_NAME $MYSQL_DATABASE --allow-root
+wp config set DB_USER $MYSQL_USER --allow-root
+wp config set DB_PASSWORD $MYSQL_ROOT_PASSWORD --allow-root
+wp config set DB_HOST "mysql:3306" --allow-root
+
+
 
 wp core install --url="$DOMAIN_NAME" --title="$TITLE" --admin_user="$ADMIN" --admin_password="$ADMIN_PWD" --admin_email="$ADMIN_EMAIL" --allow-root
 # wp theme install twentytwentytwo --allow-root
 wp user create "$WP_USR" "$WP_EMAIL" --role=author --user_pass="$WP_PWD" --allow-root
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
 
 # Start PHP-FPM
 /usr/sbin/php-fpm7.3 -F
+
+
