12e1a74f93a57ea201f1d677dd9a66cedf66b1f6
Abdelhamid Bouazi [Opus] <abouazi@student.1337.ma>
2025-03-06 03:05:15 +0000

    done switching to deban

--- inception/src/services/wordpress/setup.sh
+++ inception/src/services/wordpress/setup.sh
@@ -17,12 +17,11 @@ wp config set DB_USER $MYSQL_USER --allow-root
 wp config set DB_PASSWORD $MYSQL_ROOT_PASSWORD --allow-root
 wp config set DB_HOST "mysql" --allow-root
 
-
-
 wp core install --url="$DOMAIN_NAME" --title="$TITLE" --admin_user="$ADMIN" --admin_password="$ADMIN_PWD" --admin_email="$ADMIN_EMAIL" --allow-root
 # wp theme install twentytwentytwo --allow-root
 wp user create "$WP_USR" "$WP_EMAIL" --role=author --user_pass="$WP_PWD" --allow-root
-sed -i 's/listen = \/run\/php\/php8.3-fpm.sock/listen = 9000/' /etc/php/8.3/fpm/pool.d/www.conf
 
-mkdir /run/php
-/usr/sbin/php-fpm8.3 -F
+sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/' /etc/php/8.2/fpm/pool.d/www.conf
+
+# mkdir /run/php
+/usr/sbin/php-fpm8.2 -F
